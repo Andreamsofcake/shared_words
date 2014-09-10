acdb93eb5f76e753e08c6f68f8b4d10a13796582
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-10 10:58:30 -0700

    complete array

--- index.js
+++ index.js
@@ -3,14 +3,29 @@
  * in common.
  *
  * @param {File} Two text files
- * @param {Function} creates an array with words from the files.
+ * @param {Function} creates arrays with words from the files.
+ * @param {function} compare shared words in the arrays.
  * @param {Function} to count shared words.
- * 
+ * @returns {words} new array of all the words in common.
+ * @returns {number} number of words in the array.
  */
 
-
+/**
+ * Creates array with common words from the two input arrays.
+ * 
+ * @param {Array.<String>} array1 First array of words.
+ * @param {Array.<String>} array2 Second array of words.
+ * @returns {Array.<String>} Common words.
+ */
 module.exports.findWord = function (array1, array2) {
-	var length = array1.length
-	return length;
+	var newArray = [] 
+	array1.forEach(function(word) {
+		array2.forEach(function(word2){
+			if(word === word2) {
+				newArray.push(word);
+			}
+		});
+  	});
+	return newArray;
 };
 
--- test.js
+++ test.js
@@ -6,11 +6,12 @@ var findWord = functions.findWord
 
 
 
+
 describe('findWord()', function() {
   it('find words in common', function() {
-  	var array1 = [1];
-  	var array2 = [1];
+  	var array1 = ['Rock', 'paper', 'spock'];
+  	var array2 = ['Rock', 'lizard', 'sissors'];
   	var result = findWord(array1, array2);
-    expect(result).to.eql(1);
+    expect(result).to.eql(['Rock']);
   });
 });
